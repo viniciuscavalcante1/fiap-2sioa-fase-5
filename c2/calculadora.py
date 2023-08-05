9eb8e4012bd35e562316df73c2eadf1a3a9570a4
Vinícius Cavalcante <vcavalcanteab@gmail.com>
2023-08-05 15:42:00 -0300

    Adiciona c2 e calculadora

---
+++ c2/calculadora.py
@@ -0,0 +1,16 @@
+# Algoritmo soma
+
+valor1 = float(input("Digite o primeiro valor: "))
+valor2 = float(input("Digite o segundo valor: "))
+
+soma = valor1 + valor2
+print(f"A soma é {soma}")
+
+subtracao = valor1 - valor2
+print(f"A subtração é {subtracao}")
+
+divisao = valor1 / valor2
+print(f"A divisão é {divisao}")
+
+multiplicacao = valor1 * valor2
+print(f"A multiplicação é {multiplicacao}")
