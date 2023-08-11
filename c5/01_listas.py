b14c92ab3c2bb4a252d3c16a1e05d207038048ef
Vinícius Cavalcante <vcavalcanteab@gmail.com>
2023-08-10 22:02:54 -0300

    Adiciona capítulo 5

---
+++ c5/01_listas.py
@@ -0,0 +1,9 @@
+instrumento1 = "Guitarra"
+instrumento2 = "Piano"
+instrumento3 = "Violino"
+
+instrumentos = [instrumento1, instrumento2, instrumento3]
+print(instrumentos[1])
+
+for instrumento in instrumentos:
+    print(instrumento)
